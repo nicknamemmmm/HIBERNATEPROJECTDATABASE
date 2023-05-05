e8128726216111005efcc85e2f163ceb9a694891
nicknamemmmm <121141991+nicknamemmmm@users.noreply.github.com>
2023-05-05 15:09:53 +0530

    FIRST HIBERNATE APPLICATION DATABASE STORE
    FIRST HIBERNATE APPLICATION DATABASE STORE

---
+++ FirstHibernateApplication/src/main/java/com/soft/App.java
@@ -0,0 +1,34 @@
+package com.soft;
+
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.classic.Session;
+
+import com.soft.entity.Employee;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Configuration cnfg = new Configuration();
+       cnfg.configure("com/soft/cnfg/hibernate.cfg.xml");
+       
+       SessionFactory factory = cnfg.buildSessionFactory();
+       
+       Employee emp = new Employee();
+       emp.setEmpId(101);
+       emp.setEmpName("Aarti");
+       emp.setEmpDep("developer");
+       emp.setEmpSal(1000);
+       
+       Session session = factory.openSession(); 
+      
+       Transaction tx = session.beginTransaction();
+       session.save(emp);  
+       tx.commit();
+    }
+}
